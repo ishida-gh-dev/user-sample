5ee85426a8671a9abda60f62c11bb6c022d71244
ishida <prefabsprout@gmail.com>
2026-03-04 12:19:18 +0900

    fix: remove node_modules from git tracking

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,4 @@
+
+import { viteNodeFetch } from "file:///home/ishida/workspaces/work_nuxt/test/user-sample/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+export default () => viteNodeFetch.getManifest()
+    
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///home/ishida/workspaces/work_nuxt/test/user-sample/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
